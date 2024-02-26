f894116e957b3c162c7fd35cd1f1b813d682c431
DavitTcharbadze <tcharbadzedavit@gmail.com>
2024-02-26 12:35:05 +0400

    added advice id

--- vite-project/src/components/Advice.jsx
+++ vite-project/src/components/Advice.jsx
@@ -1,30 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import classes from '../modules/Advice.module.scss'
-import Dice from '../assets/images/icon-dice.svg'
-import Divide from '../assets/images//pattern-divider-desktop.svg'
+import React, { useState, useEffect } from 'react';
+import classes from '../modules/Advice.module.scss';
+import Dice from '../assets/images/icon-dice.svg';
+import Divide from '../assets/images/pattern-divider-desktop.svg';
 
 export default function Advice() {
-    const [advice, setAdvice] = useState('')
-    useEffect(() => {
-        getNewAdvice()
+  const [adviceId, setAdviceId] = useState(null);
+  const [adviceText, setAdviceText] = useState('');
 
-    }, [])
+  useEffect(() => {
+    getNewAdvice();
+  }, []);
 
-    const getNewAdvice = () => {
-        fetch('https://api.adviceslip.com/advice')
-            .then(response => response.json())
-            .then(data => {
-                const newAdvice = data.slip.advice;
-                setAdvice(newAdvice)
-            })
-            .catch(err => console.error(err));
-    }
+  const getNewAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => {
+        const newAdviceId = data.slip.id;
+        const newAdviceText = data.slip.advice;
+        setAdviceId(newAdviceId);
+        setAdviceText(newAdviceText);
+      })
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className={classes['advice-wrapper']}>
-        <h2>Advice #{advice.id}</h2>
-        <h1>{advice}</h1>
-        <img className={classes['divide']} src={Divide} alt="" /> <br />
-        <button onClick={getNewAdvice}><img src={Dice} alt="dice" /></button>
+      <h2>Advice #{adviceId}</h2>
+      <h1>{adviceText}</h1>
+      <img className={classes['divide']} src={Divide} alt="" /> <br />
+      <button onClick={getNewAdvice}>
+        <img src={Dice} alt="dice" />
+      </button>
     </div>
-  )
+  );
 }
